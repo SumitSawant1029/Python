dbfcfd7cd3bbaeb4c78854b6ee4fe2536172c88b
SumitSawant1029 <123000840+SumitSawant1029@users.noreply.github.com>
2023-01-23 10:59:26 -0500

    Update irrritate.py

--- irrritate.py
+++ irrritate.py
@@ -1,14 +1,18 @@
 
 # Irritating App
 from tkinter import *
-
+global i
+i=0
 root = Tk()
 # root.pack(padx=20,pady=20)
 root.title("Irritating You")
 root.iconbitmap("Hello.ico")
-
+def click1():
+    global i
+    i=i+1
+    but1 = Button(root, text="Click Me you Fool", width=20, borderwidth=9, command=click1)
+    but1.grid(column=i, row=i)
 but1 = Button(root,text= "Click Me you Fool",width=20,borderwidth=9,command=click1)
 but1.grid(column=0,row=0)
-but2 = Button(root,text= "Click Me you Fool",width=20,borderwidth=9,command=click2)
-but2.grid(column=0,row=1)
+
 root.mainloop()
