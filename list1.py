b8470c190ac35f2f39b8a5d2187c723c75d7aee0
SumitSawant1029 <123000840+SumitSawant1029@users.noreply.github.com>
2023-02-03 13:12:31 -0500

    Create list1.py

---
+++ list1.py
@@ -0,0 +1,11 @@
+L1 = [1,2,3,4,5,6,7]
+L1.pop(2) # Deletes the Last Data FRom THe LIst or at specific position
+print(L1)
+
+L1 = [1,2,3,4,5,6,7]
+L1.clear() # Clears The Whole List
+print(L1)
+
+L1 = [1,2,3,4,5,6,7]
+L1.extend()
+print(L1)
