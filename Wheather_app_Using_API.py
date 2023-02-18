3e314ab1cb2c2a4b82de32761e4c6a9052603409
SumitSawant1029 <123000840+SumitSawant1029@users.noreply.github.com>
2023-02-18 02:34:46 -0500

    Update Wheather_app_Using_API.py

--- Wheather_app_Using_API.py
+++ Wheather_app_Using_API.py
@@ -10,3 +10,28 @@ root.iconbitmap('')
 
 api_requests = requests.get('https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89120&distance=5&API_KEY=4D852C1B-5881-467D-B9F3-24FF8CAE46BA')
 api = json.loads(api_requests.content)
+
+try :
+    api_requests = requests.get('https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89120&distance=5&API_KEY=4D852C1B-5881-467D-B9F3-24FF8CAE46BA')
+    api = json.loads(api_requests.content)
+    report_area = api[0]['ReportingArea']
+    State_Code = api[0]['StateCode']
+    Latitude = api[0]['Latitude']
+    Quality = api[0]['Category']['Name']
+
+    if Quality == 'Good':
+        wheather_colour1 = '#00FF00'
+    elif Quality == 'Moderate':
+        wheather_colour1 == '#FCFF00'
+    elif Quality == 'Unhealthy for Sensitive Groups':
+        wheather_colour1 == '#FF7F00'
+    elif Quality == 'Unhealthy':
+        wheather_colour1 == '#FF001E'
+    elif Quality == 'Very Unhealthy':
+        wheather_colour1== '#C400FF'
+    elif Quality == 'Hazardous':
+        wheather_colour1== '#701111'
+    else :
+        wheather_colour1 == 'white'
+    reportarea = tkinter.Label(root, text=report_area)
+    reportarea.grid(row=0, column=0)
