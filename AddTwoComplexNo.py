cd46986877a4ed8e38a36504dc543223289577f6
SumitSawant1029 <123000840+SumitSawant1029@users.noreply.github.com>
2023-02-28 22:22:15 +0530

    Create AddTwoComplexNo.py

---
+++ AddTwoComplexNo.py
@@ -0,0 +1,10 @@
+class Complex ():
+    def initComplex(self):
+        self.realPart = int(input("Enter the Real Part: "))
+        self.imgPart = int(input("Enter the Imaginary Part: "))            
+    def display(self):
+        print(self.realPart,"+",self.imgPart,"i", sep="")
+    def sum(self, c1, c2):
+        self.realPart = c1.realPart + c2.realPart
+        self.imgPart = c1.imgPart + c2.imgPart
+        
