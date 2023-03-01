4ae46217faf099e6c80df2f34949e23f2f7c8010
SumitSawant1029 <123000840+SumitSawant1029@users.noreply.github.com>
2023-03-02 00:03:30 +0530

    Update perimeterArea.py

--- perimeterArea.py
+++ perimeterArea.py
@@ -1,19 +1,32 @@
-class triangle:
+class shape:
 	area = 0
 	perimeter = 0
 	def __init__(self):
 		print("Calculating for triangle:")
-	def Area(self,height,base):
+	def AreaT(self,height,base):
 		self.area=0.5 * height * base
+	
+	def AreaR(self,height,base):
+		self.area=height*base
+	
+	def perimeter(self,a,b):
+		self.perimeter=(2*a)+(2*b)
+		
 	def perimeter(self,a,b,c):
 		self.perimeter=a+b+c
 
 	def display(self):
 		print("The perimeter of triangle is :-",self.perimeter)
 		print("The Area of the triangle is:-",self.area)
-		
-obj1 = triangle()
 
-obj1.Area(100,29)
+
+obj1 = shape()
+
+obj1.AreaT(100,29)
 obj1.perimeter(10,20,30)
 obj1.display()
+
+obj2 = shape()
+obj2.AreaR(10,30)
+obj2.perimeter(20,12)
+obj2.display()
