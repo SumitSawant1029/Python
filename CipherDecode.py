bf8d14f15f325b93a5ea86c94ef27600756b40b7
SumitSawant1029 <123000840+SumitSawant1029@users.noreply.github.com>
2023-08-29 11:09:10 +0530

    Update CipherDecode.py

--- CipherDecode.py
+++ CipherDecode.py
@@ -1,49 +1,46 @@
 from collections import Counter
 
-
 q1 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 q2 = ['e','t','a','o','i','n','s','h','r','d','i','c','u']
 
-string="iwoo xqjg bkn haypqnao pkzwu"
-keys =[]
-string1=string
-string1=string1.replace(" ","")
+string = "slaz tlla avupnoa ha aol whyr"
+keys = []
+string1 = string
+string1 = string1.replace(" ","")
 
 # Find the highest Alphabet
-result= Counter(string1)
-result= max(result, key=result.get)
-print("Most frequent character: ",result)
+result = Counter(string1)
+result = max(result, key=result.get)
+print("Most frequent character: ", result)
 
-#Find the Index Of the largest occuring alphabet in q2 list
+# Find the Index Of the largest occurring alphabet in q2 list
 index1 = q1.index(str(result))
+print("Index of most frequent", index1)
 
-# Finding the keys difference between the charater and 
-for j in range(0,len(q2)):
+# Finding the keys difference between the character and 
+for j in range(0, len(q2)):
     index2 = q1.index(q2[j])
+    print(index2)	
     key = index2 - index1
     keys.append(key)
+print(keys)
 
-l3=[]
-# For Some Adjustments
-Adjustments=input("Enter some adjustments (Enter 0 is no):")
+l3 = []
+
 # Convert String to list
-string2=list(string)
+string2 = list(string)
+print(string2)
 
-i=0
-for j in range(0,len(q2)):
-    for i in range(0,len(string2)):
-        if string2[i] != ' ':
-            te = int(q1.index(string2[i])) - keys[j] + int(Adjustments)
-
-            l3.append(q1[te%26])
+print("Hello Herew")
+for i in range(0,len(keys)):
+    l3=[]
+    for j in range(0,len(string2)):
+        if string[j] != " ":
+            k = int(q1.index(str(string2[j]))) + int(keys[i])
+            f= k%26
+            l3.append(q1[f])
         else:
-            l3.append(' ')      
-
-    # Convert the array to a string with a specified separator
-    separator = ''  # Choose the separator you want between elements
-    result_string = separator.join(map(str, l3))
-    print(q2[j],end="")
-    print(":-",end="")
+            l3.append(" ")
+    print("For ",q2[i],":-",end=" ")
+    result_string = " ".join(l3)
     print(result_string)
-
-    l3.clear()
